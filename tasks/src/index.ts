48cbb8b97406b061dd6b3142228c72684e0623e8
Alejandro <alejandroalvareznistal.dev@gmail.com>
2025-04-01 17:37:07 +0200

    Init

---
+++ tasks/src/index.ts
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+import "./server"; // Importa y ejecuta el servidor
+
+// Hability to file .env
+dotenv.config();
---
+++ tasks/src/server.ts
@@ -0,0 +1,35 @@
+import * as grpc from "@grpc/grpc-js";
+// import dotenv from "dotenv";
+// import userProto from "./grpc/auth-loader";
+// import { AuthService } from "./services/auth-service";
+// import { AuthController } from "./controllers/auth-controller";
+// import { UserRepository } from "./repository/user-repository";
+// import { QueueService } from "./services/queue-service";
+
+// // Cargar variables de entorno
+// dotenv.config();
+
+const server = new grpc.Server();
+
+// const queueService = new QueueService();
+// const userRepository = new UserRepository();
+// const authService = new AuthService(userRepository); // Asegúrate de que AuthService está definido
+// const authController = new AuthController(authService);
+
+// server.addService(userProto.user.UserService.service,{
+//    createUser: authController.createUser.bind(authController),
+//    login: authController.login.bind(authController),
+//    verifyAccount: authController.verifyAccount.bind(authController),
+// });
+
+server.bindAsync(
+   "0.0.0.0:50051",
+   grpc.ServerCredentials.createInsecure(),
+   (err, port) => {
+      if (err) {
+         console.error("Error starting server:", err);
+         return;
+      }
+      console.log(`🚀 gRPC Server running on port ${port}`);
+   }
+);
